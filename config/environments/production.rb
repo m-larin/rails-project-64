700cc051063ceac6a7485136eb1d3f96f738ac72
m-larin <38663339+m-larin@users.noreply.github.com>
2023-01-05 12:23:19 +0300

    Config hosts clear

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,5 +91,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.hosts << ENV["RAILS_PRODUCTION_HOST"]
+  config.hosts.clear
 end
